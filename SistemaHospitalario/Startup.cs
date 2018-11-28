b04efd3af9fda7fc462e36be53a41b0ad38f2def
Danieldeleon <danielitolf@gmail.com>
2018-11-27 20:37:35 -0400

    Agregando MVC al proyecto

--- SistemaHospitalario/Startup.cs
+++ SistemaHospitalario/Startup.cs
@@ -15,6 +15,7 @@ namespace SistemaHospitalario
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,15 @@ namespace SistemaHospitalario
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc(config=>config.MapRoute(
+                "Default",
+                "{controller}/{action}/{id?}",
+                new {
+                    Controller="Home",
+                    Action="Index"
+                }
+            ));
 
             app.Run(async (context) =>
             {
